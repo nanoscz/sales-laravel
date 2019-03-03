3650045e1989d0c7d666a326828860e30a570fb1
unknown <fernandocto.scz@gmail.com>
2019-03-03 14:54:20 -0600

    add article vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,7 @@ window.Vue = require('vue');
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('category', require('./components/Category.vue'));
+Vue.component('articles', require('./components/Article.vue'));
 const app = new Vue({
     el: '#app',
     data: {
