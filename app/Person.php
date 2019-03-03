9ac66ad34ff52d7c6135e7688e3014632d071acf
unknown <fernandocto.scz@gmail.com>
2019-03-03 16:17:52 -0600

    add the person model

---
+++ app/Person.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    protected $table = 'person';
+    protected $fillable = ['name','type_document','num_document','direction','phone','email'];
+}
