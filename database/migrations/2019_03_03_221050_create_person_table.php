558dd04d648d6db17b6c03e54784c1c09feba0fb
unknown <fernandocto.scz@gmail.com>
2019-03-03 16:17:41 -0600

    add the person migration

---
+++ database/migrations/2019_03_03_221050_create_person_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->unique();
+            $table->string('type_document', 20)->nullable();
+            $table->string('num_document', 20)->nullable();
+            $table->string('direction', 70)->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->string('email', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person');
+    }
+}
