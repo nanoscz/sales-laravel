4733f19be8a1c2a6d18cbf95a606ddb9f78db6a5
unknown <fernandocto.scz@gmail.com>
2019-03-03 16:33:19 -0600

    fix client migration

--- database/migrations/2019_03_03_221050_create_person_table.php
+++ database/migrations/2019_03_03_221050_create_person_table.php
@@ -15,9 +15,9 @@ class CreatePersonTable extends Migration
     {
         Schema::create('person', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 100)->unique();
+            $table->string('name', 100);
             $table->string('type_document', 20)->nullable();
-            $table->string('num_document', 20)->nullable();
+            $table->string('num_document', 20)->unique();
             $table->string('direction', 70)->nullable();
             $table->string('phone', 20)->nullable();
             $table->string('email', 50)->nullable();
