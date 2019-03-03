ccc73bf4dc2bcf708d75773aab603f1562a63099
unknown <fernandocto.scz@gmail.com>
2019-03-03 14:18:34 -0600

    add controller article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,19 +12,32 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
+    public function index(Request $request)
+    {   
+        if (!$request->ajax()) return redirect('/');
+        $search = $request->search;
+        $option = $request->option;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        if ($search == '') {
+            $articles = Article::join('category','article.id_category','=','category.id')
+            ->select('article.id','article.code','article.name','article.price','article.stock','article.description','article.state','article.id_category','category.name as category')
+            ->orderBy('article.id','desc')->paginate(10);
+        } else {
+            $articles = Article::join('category','article.id_category','=','category.id')
+            ->select('article.id','article.code','article.name','article.price','article.stock','article.description','article.state','article.id_category','category.name as category')
+            ->where('article.'.$option,'like','%'. $search .'%')->orderBy('id','desc')->paginate(10);
+        }
+        return [
+            'pagination' => [
+                'total' => $articles->total(),
+                'current_page' => $articles->currentPage(),
+                'per_page' => $articles->perPage(),
+                'last_page' => $articles->lastPage(),
+                'from' => $articles->firstItem(),
+                'to' => $articles->lastItem()
+            ],
+            'categories' => $articles
+        ];
     }
 
     /**
@@ -35,51 +48,54 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Article $article)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article)
-    {
-        //
+        if (!$request->ajax()) return redirect('/');
+        $article = new Article();
+        $article->id_category = $request->id_category;
+        $article->code = $request->code;
+        $article->name = $request->name;
+        $article->price = $request->price;
+        $article->stock = $request->stock;
+        $article->description = $request->description;
+        $article->state = '1';
+        $article->save();
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Article  $article
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Article $article)
+    public function update(Request $request)
     {
-        //
+        if (!$request->ajax()) return redirect('/');
+        $article = Article::findOrFail($request->id);
+        $article->id_category = $request->id_category;
+        $article->code = $request->code;
+        $article->name = $request->name;
+        $article->price = $request->price;
+        $article->stock = $request->stock;
+        $article->description = $request->description;
+        $article->state = 1;
+        $article->save();
     }
 
     /**
-     * Remove the specified resource from storage.
+     * changeState the specified resource in storage.
      *
-     * @param  \App\Article  $article
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Article $article)
+    public function changeState(Request $request)
     {
-        //
+        if (!$request->ajax()) return redirect('/');
+        $article = Category::findOrFail($request->id);
+        if ($article->state == '1') {
+            $article->state = '0';
+        } else {
+            $article->state = '1';
+        }
+        $article->save();
     }
 }
