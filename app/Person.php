fb2f8e00836a2c48c6c7767070529854b1f509b7
unknown <fernandocto.scz@gmail.com>
2019-03-03 19:16:46 -0600

    add foreign key in person

--- app/Person.php
+++ app/Person.php
@@ -8,4 +8,8 @@ class Person extends Model
 {
     protected $table = 'person';
     protected $fillable = ['name','type_document','num_document','direction','phone','email'];
+
+    public function proveedor(){
+        return $this->hasOne('App\Provider');
+    }
 }
