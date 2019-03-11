7d0b4b50d26ad0a586307a6e4eda0286e85d4651
unknown <fernandocto.scz@gmail.com>
2019-03-11 00:42:54 -0600

    fix income component

--- resources/assets/js/components/Income.vue
+++ resources/assets/js/components/Income.vue
@@ -13,10 +13,7 @@
       <div class="card">
         <div class="card-header">
           <i class="fa fa-align-justify"></i> Income
-          <button
-            type="button"
-            class="btn btn-secondary"
-            @click="showDetail()">
+          <button type="button" class="btn btn-secondary" @click="showDetail()">
             <i class="icon-plus"></i>&nbsp;Add
           </button>
         </div>
@@ -128,21 +125,20 @@
           </div>
         </template>
 
-        <!-- detail -->
+        <!-- add income -->
         <template v-else>
           <div class="card-body">
             <div class="form-group row border">
               <div class="col-md-9">
                 <div class="form-group">
                   <label for="provider">Provider</label>
-                 <v-select
+                  <v-select
                     :on-search="selectProviders"
                     label="name"
                     :options="providers"
                     placeholder="Search provides..."
-                    :onChange="getProvider">
-
-                  </v-select>
+                    :onChange="getProvider"
+                  ></v-select>
                 </div>
               </div>
               <div class="col-md-3">
@@ -181,28 +177,47 @@
                   >
                 </div>
               </div>
+              <div class="col-md-12">
+                <div v-show="err" class="form-group row div-error">
+								  <div class="text-center text-error">
+									 <div v-for="error in listErr" :key="error" v-text="error">
+									 </div>
+								  </div>
+							  </div> 
+              </div>
             </div>
             <div class="form-group row border">
               <div class="col-md-6">
                 <div class="form-group">
-                  <label>Article <span style="color:red" v-show="id_article == 0">(*)</span></label>
+                  <label>Article
+                    <span style="color:red" v-show="id_article == 0">(*)</span>
+                  </label>
                   <div class="form-inline">
-                    <input type="text" class="form-control" v-model="code" placeholder="article"
-                      @keyup.enter="getArticles()">
-                    <button class="btn btn-primary">...</button>
+                    <input
+                      type="text"
+                      class="form-control"
+                      v-model="code"
+                      placeholder="article"
+                      @keyup.enter="getArticles()"
+                    >
+                    <button class="btn btn-primary" @click="openModal()">...</button>
                     <input type="text" readonly class="form-control" v-model="article">
                   </div>
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="form-group">
-                  <label for="price">Price <span style="color:red" v-show="price == 0">(*)</span></label>
+                  <label for="price">Price
+                    <span style="color:red" v-show="price == 0">(*)</span>
+                  </label>
                   <input type="number" step="any" value="0" class="form-control" v-model="price">
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="form-group">
-                  <label for="quantity">Quantity <span style="color:red" v-show="quantity == 0">(*)</span></label>
+                  <label for="quantity">Quantity
+                    <span style="color:red" v-show="quantity == 0">(*)</span>
+                  </label>
                   <input type="number" step="any" value="0" class="form-control" v-model="quantity">
                 </div>
               </div>
@@ -229,7 +244,11 @@
                   <tbody v-if="details.length">
                     <tr v-for="(detail, index) in details" :key="detail.id">
                       <td>
-                        <button type="button" class="btn btn-danger btn-sm" @click="removeArticleDetail(index)">
+                        <button
+                          type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="removeArticleDetail(index)"
+                        >
                           <div class="icon-close"></div>
                         </button>
                       </td>
@@ -262,7 +281,7 @@
                     </tr>
                   </tbody>
                   <tbody>
-                    <h3> Not Found</h3>
+                    <h3>Not Found</h3>
                   </tbody>
                 </table>
               </div>
@@ -286,7 +305,8 @@
       aria-labelledby="myModalLabel"
       style="display: none;"
       aria-hidden="true"
-      :class="{'show-modal': modal}">
+      :class="{'show-modal': modal}"
+    >
       <div class="modal-dialog modal-primary modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -295,15 +315,75 @@
               <span aria-hidden="true">×</span>
             </button>
           </div>
-          <div class="modal-body"></div>
+          <div class="modal-body">
+            <!-- modalarticles -->
+             <div class="form-group row">
+              <div class="col-md-6">
+              <div class="input-group">
+                <select class="form-control col-md-3" v-model="optionArticles">
+									<option value="code">Code</option>
+                  <option value="name">Name</option>
+                  <option value="description">Description</option>
+                </select>
+                <input
+                  type="text"
+                  v-model="searchArticles"
+                  class="form-control"
+									@keyup.enter="showArticles(searchArticles, optionArticles)"
+                  placeholder="Seach..."
+                >
+                <button type="submit" class="btn btn-primary" @click="showArticles(searchArticles, optionArticles)">
+                  <i class="fa fa-search"></i> Search
+                </button>
+              </div>
+            </div>
+            </div>
+            <div class="table-responsive">
+              <table class="table table-bordered table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th>Option</th>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                    <th>State</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="article in articles" :key="article.id">
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-success btn-sm"
+                        @click="addDetailArticleModal(article)"
+                      >
+                        <i class="icon-check"></i>
+                      </button>
+                    </td>
+                    <td v-text="article.code"></td>
+                    <td v-text="article.name"></td>
+                    <td v-text="article.category"></td>
+                    <td v-text="article.description"></td>
+                    <td v-text="article.price"></td>
+                    <td v-text="article.stock"></td>
+                    <td>
+                      <div v-if="article.state">
+                        <span @click="changeState(article.id)" class="badge badge-success">active</span>
+                      </div>
+                      <div v-else>
+                        <span @click="changeState(article.id)" class="badge badge-danger">desactive</span>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" @click="closeModal()">Cerrar</button>
-            <div v-if="typeAction == 1">
-              <button type="button" class="btn btn-success" @click="save()">Save</button>
-            </div>
-            <div v-if="typeAction == 2">
-              <button type="button" class="btn btn-primary" @click="update()">Update</button>
-            </div>
           </div>
         </div>
         <!-- /.modal-content -->
@@ -346,27 +426,26 @@
 </template>
 
 <script>
-import vSelect from 'vue-select';
+import vSelect from "vue-select";
 export default {
   data() {
     return {
       id: 0,
       id_provider: 0,
-      name: "",
       type_voucher: "BILL",
       series_voucher: "",
       num_voucher: "",
       tax: 0.18,
       total: 0,
-      code:'',
-      id_article:'',
-      article:'',
+      code: "",
+      id_article: 0,
+      article: "",
       price: 0,
       quantity: 0,
       incomes: [],
       details: [],
       providers: [],
-      articles:[],
+      articles: [],
       list: 1,
       modal: 0,
       titleModal: "",
@@ -385,7 +464,9 @@ export default {
       totalPartial: 0.0,
       offset: 3,
       option: "num_voucher",
-      search: ""
+      search: "",
+      optionArticles: "name",
+      searchArticles: ""
     };
   },
   components: {
@@ -417,10 +498,10 @@ export default {
       }
       return pagesArray;
     },
-    calculateTotal: function(){
+    calculateTotal: function() {
       let total = 0;
       for (let i = 0; i < this.details.length; i++) {
-        total = total + (this.details[i].price * this.details[i].quantity);   
+        total = total + this.details[i].price * this.details[i].quantity;
       }
       return total;
     }
@@ -441,13 +522,14 @@ export default {
           console.error(err);
         });
     },
-    getRoles() {
+    showArticles(searchArticles, optionArticles){
       let ctrl = this;
+			let url = '/articles/modal?search=' + searchArticles + '&option=' + optionArticles;
       axios
-        .get("/roles")
+        .get(url)
         .then(function(res) {
-          let response = res.data;
-          ctrl.roles = response.roles;
+					let response = res.data;
+          ctrl.articles = response.articles.data;
         })
         .catch(function(err) {
           console.error(err);
@@ -462,101 +544,76 @@ export default {
     },
     validate() {
       this.cleanError();
-      if (!this.name) this.listErr.push("Field required name!");
-      if (!this.type_document)
-        this.listErr.push("Field required document type!");
-      if (!this.num_document)
-        this.listErr.push("Field required document number !");
+      if (this.id_provider == 0) this.listErr.push("Select Provider");
+      if (this.type_voucher == 0) this.listErr.push("Field required type voucher");
+      if (this.series_voucher == 0) this.listErr.push("Field required series voucher");
+      if (!this.num_voucher) this.listErr.push("Field required number voucher");
+      if (!this.tax) this.listErr.push("Field required tax!");
+      if (this.details.length <= 0) this.listErr.push("Field required details articels!");
       if (this.listErr.length) this.err = 1;
       return this.err;
     },
-    save() {
+    registerIncome() {
       if (this.validate()) {
         return;
       }
       let ctrl = this;
       let data = {
-        name: ctrl.name,
-        type_document: ctrl.type_document,
-        num_document: ctrl.num_document,
-        direction: ctrl.direction,
-        phone: ctrl.phone,
-        email: ctrl.email,
-        username: ctrl.username,
-        password: ctrl.password,
-        id_rol: ctrl.id_rol
+        id_provider: ctrl.id_provider,
+        type_voucher: ctrl.type_voucher,
+        series_voucher: ctrl.series_voucher,
+        num_voucher: ctrl.num_voucher,
+        tax: ctrl.tax,
+        total: ctrl.total,
+        data: ctrl.details
       };
+      console.log({data});
       axios
         .post("/income", data)
         .then(function(res) {
-          // me.Incomes.push(data)
+          ctrl.list = 1;
           ctrl.show(1, "", "name");
-          ctrl.closeModal();
+          ctrl.cleanForm();
           console.log(res);
         })
         .catch(function(err) {
           console.error("error al register", err);
         });
     },
-    update() {
-      if (this.validate()) {
-        return;
-      }
-      let ctrl = this;
-      let data = {
-        id: ctrl.id,
-        name: ctrl.name,
-        type_document: ctrl.type_document,
-        num_document: ctrl.num_document,
-        direction: ctrl.direction,
-        phone: ctrl.phone,
-        email: ctrl.email,
-        username: ctrl.username,
-        password: ctrl.password,
-        id_rol: ctrl.id_rol
-      };
+    selectProviders(search, loading) {
+      let me = this;
+      loading(true);
+
+      var url = "/providers?filter=" + search;
       axios
-        .put("/Income", data)
+        .get(url)
         .then(function(res) {
-          ctrl.show(1, "", "name");
-          ctrl.closeModal();
-          console.log(res);
+          let response = res.data;
+          q: search;
+          me.providers = response.providers;
+          loading(false);
         })
-        .catch(function(err) {
-          console.error(err);
+        .catch(function(error) {
+          console.error(error);
         });
     },
-    selectProviders(search,loading){
-      let me=this;
-      loading(true)
-
-      var url= '/providers?filter=' + search;
-      axios.get(url).then(function (res) {
-          let response = res.data;
-          q: search
-          me.providers = response.providers;
-          loading(false)
-      })
-      .catch(function (error) {
-          console.error(error);
-      });
-    },
-    getProvider(val1){
+    getProvider(val1) {
       let me = this;
       me.loading = true;
       me.id_provider = val1.id;
     },
-    getArticles(){
+    getArticles() {
       let me = this;
-      let url = '/articles?filter=' + me.code;
-      axios.get(url)
+      let url = "/articles?filter=" + me.code;
+      axios
+        .get(url)
         .then(function(response) {
           me.articles = response.data.articles;
           if (me.articles.length > 0) {
-            me.article = me.articles[0]['name']
-            me.id_article = me.articles[0]['id']
+            me.article = me.articles[0]["name"];
+            me.id_article = me.articles[0]["id"];
           } else {
-            me.article = 'not found'
+            me.article = "not found";
             me.id_article = 0;
           }
         })
@@ -564,103 +621,95 @@ export default {
           console.error(err);
         });
     },
-    exists(id){
-      let exists = false; 
+    exists(id) {
+      let exists = false;
       if (this.details.length) {
         for (let i = 0; i < this.details.length; i++) {
           if (this.details[i].id_article == id) {
             exists = true;
-          }          
+          }
         }
       }
       return exists;
     },
-    removeArticleDetail(index){
+    removeArticleDetail(index) {
       let ctrl = this;
       ctrl.details.splice(index, 1);
     },
-    addArticleDetail(){
+    addArticleDetail() {
       let ctrl = this;
-      if (ctrl.id_article !== 0 || ctrl.quantity !== 0 || ctrl.price !== 0) {
+      if (ctrl.id_article !== 0 && ctrl.quantity !== 0 && ctrl.price !== 0) {
         if (ctrl.exists(ctrl.id_article)) {
           Swal.fire({
-            type: 'error',
-            title: 'Error...',
-            text: 'The article has already been added'
+            type: "error",
+            title: "Error...",
+            text: "The article has already been added"
             // footer: '<a href>Why do I have this issue?</a>'
           });
         } else {
-            ctrl.details.push({
+          ctrl.details.push({
             id_article: ctrl.id_article,
             article: ctrl.article,
             quantity: ctrl.quantity,
             price: ctrl.price
           });
         }
+      } else {
+        console.log("Required id_article", ctrl.id_article);
+        console.log("Required article", ctrl.article);
+        console.log("Required quantity", ctrl.quantity);
+        console.log("Required price", ctrl.price);
       }
     },
-    openModal(model, action, data = []) {
-      switch (model) {
-        case "Income":
-          {
-            switch (action) {
-              case "register": {
-                this.cleanError();
-                this.modal = 1;
-                this.typeAction = 1;
-                this.titleModal = "Register Income";
-                this.name = "";
-                this.type_document = "";
-                this.num_document = "";
-                this.direction = "";
-                this.phone = "";
-                this.email = "";
-                this.username = "";
-                this.password = "";
-                this.id_rol = 0;
-                break;
-              }
-              case "update": {
-                this.modal = 1;
-                this.typeAction = 2;
-                this.titleModal = "Update Income";
-                this.id = data["id"];
-                this.name = data["name"];
-                this.type_document = data["type_document"];
-                this.num_document = data["num_document"];
-                this.direction = data["direction"];
-                this.phone = data["phone"];
-                this.email = data["email"];
-                this.username = data["username"];
-                this.password = data["password"];
-                this.id_rol = data["id_rol"];
-              }
-            }
-          }
-          this.getRoles();
-      }
+    addDetailArticleModal(data = []){
+      let ctrl = this;
+      if (ctrl.exists(data['id'])) {
+          Swal.fire({
+            type: "error",
+            title: "Error...",
+            text: "The article has already been added"
+            // footer: '<a href>Why do I have this issue?</a>'
+          });
+        } else {
+          ctrl.details.push({
+            id_article: data['id'],
+            article: data['name'],
+            quantity: 1,
+            price:1
+          });
+        }
     },
-    showDetail(){
+    openModal(model) {
+      this.articles = [];
+      this.modal = 1;
+      this.titleModal = "Select one or more articles.";
+    },
+    showDetail() {
       this.list = 0;
     },
-    closeDetail(){
+    closeDetail() {
       this.list = 1;
     },
     closeModal() {
       this.modal = 0;
       this.titleModal = "";
-      this.name = "";
-      this.type_document = "";
-      this.num_document = "";
-      this.direction = "";
-      this.phone = "";
-      this.email = "";
-      this.username = "";
-      this.id_rol = 0;
     },
     cleanError() {
       this.err = 0;
       this.listErr = [];
+    },
+    cleanForm(){
+      let ctrl = this;
+      ctrl.id_provider = 0;
+      ctrl.type_voucher = 'BILL';
+      ctrl.series_voucher = '';
+      ctrl.num_voucher = '';
+      ctrl.tax = 0.18;
+      ctrl.total = 0.0;
+      ctrl.article = '';
+      ctrl.price = 0;
+      ctrl.quantity = 0;
+      ctrl.details = [];
     },
     changeState(id) {
       console.log("Income", id);
